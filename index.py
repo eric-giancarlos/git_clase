225770c4877f4b7e22115f0ac3d74ad1be7106cd
eric-giancarlos <eric_giancarlos@outlook.com>
2024-02-14 18:25:21 -0500

    se modifica archivo index.py

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-print("Hola nuevos programadores")
+print("¡¡¡Hola nuevos programadores de ToulouseL!!!")
+
+print("Esta es la clase de GitHub")
+
+print("Trabajaremos con ramas (branch)")
+
+print("Trabajaremos con ramas (branch) en desarrollo")
